acb4d0b6301f51fea7f0464a8df0f06ca7a8ef42
shahed-emsl <151946873+shahed-emsl@users.noreply.github.com>
2024-03-28 19:14:54 +0600

    change some cli

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hoqfp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.len0asq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+// mongodb+srv://mdbinshahed5:<password>@cluster0.len0asq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true
